266339266a364d29cb8e3c84e936e71c44fa0dfd
Home PC <sindzi0007@yandex.ru>
2020-09-19 17:55:01 +0300

    Updates

---
+++ precache-manifest.e1ca0a2542f54d8590f9ca4fea41d645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816f7157b2718f9a464cc5b1f5468c93",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "952c9c7ac1b16e66bd92",
+    "url": "/messenger/static/css/main.c50ec399.chunk.css"
+  },
+  {
+    "revision": "975351834e2577a686be",
+    "url": "/messenger/static/js/2.829445df.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/messenger/static/js/2.829445df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952c9c7ac1b16e66bd92",
+    "url": "/messenger/static/js/main.22dcda7d.chunk.js"
+  },
+  {
+    "revision": "42b02ae745a4878b278e",
+    "url": "/messenger/static/js/runtime-main.68fb1750.js"
+  }
+]);
